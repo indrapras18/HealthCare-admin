39dfbeb8fab30f6dd17a7baa2cf701bdfd689029
IndraPras <indrapras2002@gmail.com>
2022-11-27 19:27:33 +0700

    add api

--- api/getPerawat.php
+++ api/getPerawat.php
@@ -1,5 +1,4 @@
 <?php
-// view data
 include '../core/koneksi.php';
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
@@ -9,7 +8,7 @@ if (empty($_GET)) {
     $result = array();
     while ($row = mysqli_fetch_array($query)) {
         array_push($result, array(
-            'id_tenaga_medis' => $row['id_tenaga_medis'],
+            'id_tenagamedis' => $row['id_tenagamedis'],
             'nama' => $row['nama'],
             'email' => $row['email'],
             'no_str' => $row['no_str'],
@@ -22,11 +21,11 @@ if (empty($_GET)) {
         array('result' => $result)
     );
 } else {
-    $query = mysqli_query($koneksi, "SELECT * FROM userr where id =". $_GET['id']);
+    $query = mysqli_query($koneksi, "SELECT * FROM tenaga_medis where id_tenagamedis =". $_GET['id_tenagamedis']);
     $result = array();
     while ($row = $query->fetch_assoc()) {
         $result = array(
-            'id_tenaga_medis' => $row['id_tenaga_medis'],
+            'id_tenagamedis' => $row['id_tenagamedis'],
             'nama' => $row['nama'],
             'email' => $row['email'],
             'no_str' => $row['no_str'],
--- api/login.php
+++ api/login.php
@@ -10,7 +10,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $email = $_POST['email'];
     $password = ($_POST['password']);
     
-    $cek = "SELECT * FROM users WHERE email='$email' and password='$password'";
+    $cek = "SELECT * FROM pasien WHERE email='$email' and password='$password'";
     $result = mysqli_fetch_array(mysqli_query($koneksi, $cek));
 
     if(isset($result)){
